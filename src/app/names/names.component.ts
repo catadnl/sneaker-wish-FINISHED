15921d4907212208e0e39aaf8ef417ceb8e834c9
Catalin Matei <matei.catalin25@gmail.com>
2018-08-05 13:00:41 +0300

    # Introduction Tasks
    * Add bootstrap dependency and change the angular.json file
    * Create new component and analyze the Decorator options
    * Change the template off AppComponent to include the new component
    * Declare it on the App Module and talk about modularity in Angular
    * Use inline styles and templates and external also

---
+++ src/app/names/names.component.ts
@@ -0,0 +1,24 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-names',
+  // template: `
+  //   <div class="col-xs-12">
+  //     <h2 class="pretty-color">Names list is loaded</h2>
+  //   </div>
+  // `,
+  // styles: [`
+  //   .pretty-color {
+  //     display: inline;
+  //     color: lightcoral;
+  //     background-color: lightblue;
+  //   }
+  // `]
+  templateUrl: './names.component.html',
+  styleUrls: ['./names.component.css'],
+})
+export class NamesComponent {
+
+  names: ['Gica', 'Andrei', 'Alex'];
+
+}
